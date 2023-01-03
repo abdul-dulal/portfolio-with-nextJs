d268031358428e78f3a65f5c1f825abd5d1dd407
abdul-dulal <dulal.dpi.387364@gmail.com>
2023-01-03 09:46:17 +0600

    deploy error2

--- components/navbar.js
+++ components/navbar.js
@@ -8,7 +8,7 @@ export default function Navbar(params) {
     if (pathname === "/") return setActiveItems("About");
     if (pathname === "/projects") return setActiveItems("Projects");
     if (pathname === "/resume") return setActiveItems("Resume");
-  }, []);
+  }, [pathname]);
   return (
     <div className="flex justify-between py-3 my-3 px-5">
       <span className="font-bold text-green md:text-2xl text-xl border-b-4 border-green ">
